5f0f2aae6bcb4ea37a8e89c749e6fab5256cb030
BAQACH Adil <baqachadil@gmail.com>
2021-06-01 18:28:37 +0100

    Xrays_Image Upload

--- src/app/Components/xrays/xrays.component.ts
+++ src/app/Components/xrays/xrays.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { FormControl, FormGroup, NgForm, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-xrays",
@@ -7,25 +8,18 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./xrays.component.css"],
 })
 export class XraysComponent implements OnInit {
-  fileName = "";
+  fileData: File = null;
 
   constructor(private http: HttpClient) {}
 
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+  }
   ngOnInit() {}
 
-  onFileSelected(event) {
-    const file: File = event.target.files[0];
-
-    if (file) {
-      this.fileName = file.name;
-
-      const formData = new FormData();
-
-      formData.append("thumbnail", file);
-
-      const upload$ = this.http.post("/api/thumbnail-upload", formData);
-
-      upload$.subscribe();
-    }
+  onSubmit() {
+    let data: FormData = new FormData();
+    data.append("image", this.fileData);
+    console.log(this.fileData);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
-import { XraysComponent } from './Components/xrays/xrays.component';
-import { SymptomsComponent } from './Components/symptoms/symptoms.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./Components/home/home.component";
+import { XraysComponent } from "./Components/xrays/xrays.component";
+import { SymptomsComponent } from "./Components/symptoms/symptoms.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     XraysComponent,
-    SymptomsComponent
+    SymptomsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
